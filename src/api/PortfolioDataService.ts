562d1ca1434aaf055920919ff0ab6c156c60361e
tpho216 <tpho216@gmail.com>
2021-12-29 20:54:10 +1100

    update project service for API CRUD calls

--- src/api/PortfolioDataService.ts
+++ src/api/PortfolioDataService.ts
@@ -1,5 +1,6 @@
 import { AxiosInstance} from "axios";
 import { IProject } from "./interfaces/IProject";
+import {ISkill} from "./interfaces/ISkill";
 
 class PortfolioDataService {
   private readonly axios: AxiosInstance;
@@ -7,10 +8,52 @@ class PortfolioDataService {
   constructor(axiosInstance: AxiosInstance) {
     this.axios = axiosInstance;
   }
-
-  public async getProjects() {
-    return this.axios.get<IProject[]>('/project');
+  public async createProject(project : IProject) {
+    try {
+      return this.axios.post<IProject>('/project', project);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
   }
+
+  public async fetchProjects() {
+    try {
+      return this.axios.get<IProject[]>('/project');
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  public async updateProject(project: IProject) {
+    try {
+      return this.axios.put(`/project/${project.id}`, project);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  public async deleteProject(project: IProject) {
+    try {
+      return this.axios.delete(`/project/${project.id}`);
+    } catch (e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+
+
+  public async getSkills() {
+    return this.axios.get<ISkill[]>('/skill');
+  }
+
+  public async updateSkill(skill: ISkill) {
+    return this.axios.put(`/skill/${skill.id}`, skill);
+  }
+
 }
 
 export default PortfolioDataService;
