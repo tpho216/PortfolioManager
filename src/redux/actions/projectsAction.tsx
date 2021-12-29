e2a48c70dffda6e3919d0db2e3fb14046c2d480c
tpho216 <tpho216@gmail.com>
2021-12-29 20:49:44 +1100

    added common dispatch actions

--- src/redux/actions/projectsAction.tsx
+++ src/redux/actions/projectsAction.tsx
@@ -1,4 +1,54 @@
 import APIClient from "../../api/APIClient";
-import axios from "axios";
+import {IProject} from "../../api/interfaces/IProject";
+import {
+    CREATE_PROJECT_ITEM_ACTION,
+    DELETE_PROJECT_ITEM_ACTION,
+    FETCH_PROJECTS_DATA_ACTION,
+    UPDATE_PROJECT_ITEM_ACTION
+} from "../../common/types";
 
-export const FETCH_PROJECTS = "FETCH_PROJECTS_DATA";
+
+export const fetchProjects = (projects : IProject[]) => async (dispatch : any) => {
+    try {
+        dispatch({ type: FETCH_PROJECTS_DATA_ACTION, payload: projects});
+    } catch (e) {
+        console.log("error:", e);
+        throw e;
+    }
+}
+
+export const updateProjectItem = (projectsArray : IProject[], project: IProject,
+                                         ) => async (dispatch : any) => {
+    try {
+        projectsArray.map((projectItem : IProject, index: number) => {
+            if (projectItem.id === project.id) {
+                dispatch({ type: UPDATE_PROJECT_ITEM_ACTION, payload: {project: project, index: index}});
+            }
+        })
+    } catch (e) {
+        console.log("error:", e);
+        throw e;
+    }
+}
+
+export const deleteProjectItem = (projectsArray : IProject[], project : IProject) => async (dispatch: any) => {
+    try {
+        projectsArray.map((projectItem : IProject, index : number) => {
+            if (projectItem.id === project.id) {
+                dispatch({ type: DELETE_PROJECT_ITEM_ACTION, payload: {index: index}})
+            }
+        });
+    } catch (e) {
+        console.log("error:", e);
+        throw e;
+    }
+}
+
+export const addProjectItem = (project : IProject) => async (dispatch : any) => {
+    try {
+        dispatch({type: CREATE_PROJECT_ITEM_ACTION, payload: {project: project}});
+    } catch (e) {
+        console.log("error: ", e);
+        throw e;
+    }
+}
