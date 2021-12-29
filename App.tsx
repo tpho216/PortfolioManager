04544103e9dfc90d4879288c32c5b8c8e7130ffb
tpho216 <tpho216@gmail.com>
2021-12-29 20:54:58 +1100

    added swipe to refresh controls

--- App.tsx
+++ App.tsx
@@ -1,33 +1,54 @@
 import React from "react";
-import { SafeAreaView, ScrollView, StatusBar, useColorScheme, View } from "react-native";
+import {RefreshControl, SafeAreaView, ScrollView, StatusBar, StyleSheet, useColorScheme, View} from "react-native";
 import { Colors } from "react-native/Libraries/NewAppScreen";
 import { Header } from "./src/components/Header/Header";
-import { Section } from "./src/components/Section/Section";
-import { ProjectsSection } from "./src/components/Section/ProjectsSection";
+import { ProjectsSection } from "./src/components/Section/Project/ProjectsSection";
+import {SkillsSection} from "./src/components/Section/SkillsSection";
+import {PortfolioActionButtonsGroup} from "./src/components/PortfolioAction/PortfolioActionButtonsGroup";
+import {useDispatch} from "react-redux";
+import {fetchProjects} from "./src/redux/actions/projectsAction";
+import APIClient from "./src/api/APIClient";
 
 const App = () => {
-
+  const [refreshing, setRefreshing] = React.useState(false);
   const isDarkMode = useColorScheme() === 'dark';
+  const apiClient : APIClient = new APIClient();
+  const dispatch = useDispatch();
 
-  const backgroundStyle = {
+    const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    const projectsResponse = await apiClient.portfolioDataService.fetchProjects();
+    dispatch(fetchProjects(projectsResponse.data));
+    setRefreshing(false);
+
+  }, [])
+
+
+
   return (
 
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic">
+        contentInsetAdjustmentBehavior="automatic"
+        refreshControl={
+          <RefreshControl refreshing={refreshing}
+            onRefresh={onRefresh}/>
+        }
+      >
         <Header/>
-        <View>
-          <ProjectsSection title="My Projects"></ProjectsSection>
-          <Section title="Services"></Section>
-        </View>
+        <ProjectsSection title="My Projects"/>
+        <SkillsSection title="My Skills"/>
       </ScrollView>
-
+        <PortfolioActionButtonsGroup/>
     </SafeAreaView>
-
   );
 };
+
+
 
 export default App;
