785dbe847209017b0c5cd877d53ebae228fdf842
tpho216 <tpho216@gmail.com>
2021-12-29 20:45:55 +1100

    updated reducers

--- src/redux/reducers/rootReducer.ts
+++ src/redux/reducers/rootReducer.ts
@@ -1,11 +1,14 @@
 import { combineReducers} from "redux";
 import projectsReducer, { projectsState} from "./projectsReducer";
+import skillsReducer, {skillsState} from "./skillsReducer";
 
 export interface RootState {
-  projects: projectsState
+  projects: projectsState,
+  skills: skillsState
 }
 export const rootReducer = combineReducers<RootState>({
   projects : projectsReducer,
+  skills : skillsReducer,
 });
 
 export default rootReducer;
